417859c3a64bc633c64eab9b682573988c7e4bc0
Dylan Hedges <d.hedges1@hotmail.co.uk>
2023-11-14 08:51:57 -0700

    Added content to Contact page

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -8,6 +8,21 @@ export default function Contact() {
             <div className={styles.introContainer}>
                 <TypingAnimation page='Contact' />
             </div>
+            <div className={styles.contentContainer}>
+                <div className={styles.textSection}>
+                    <h1>Let's Chat!</h1>
+                    <h2>Get in touch.</h2>
+                    <h4>Email:</h4>
+                    <p>dh@dylanhedges.com</p>
+                    <h4>LinkedIn:</h4>
+                    <p>linkedin.com/in/dylan-hedges/</p>
+                    <h4>GitHub:</h4>
+                    <p>github.com/Dylan-Hedges</p>
+                </div>
+                <div className={styles.imageSection}>
+                    <img  className={styles.image}src="/images/contact.jpg" alt=""/>
+                </div>
+            </div>
         </main>
     )
 }
